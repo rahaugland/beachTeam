1990955c3bc6f77b69c25ac0c9b96f6819815aac
Rasmus Longva Haugland <rasmus.longva.haugland@gyldendal.no>
2023-07-31 22:47:08 +0200

    Added navarb and some basic css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 import volleyball from "./volleyball.svg";
+import Navbar from './components/Header.tsx';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={volleyball} className="App-logo" alt="logo" />
         <p>
