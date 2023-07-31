3233aa54dd0b7a3506215a7778d2199c4fea9492
Rasmus Longva Haugland <rasmus.longva.haugland@gyldendal.no>
2023-07-31 21:43:12 +0200

    minor change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WIP: Homepage for NTNUI elite beach 
         </p>
         <a
           className="App-link"
